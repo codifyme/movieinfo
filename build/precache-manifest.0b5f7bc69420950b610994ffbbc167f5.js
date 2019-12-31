029ce96a764e3c56c9aa93c4c4f1e804c32ce862
Angel Cochachez <acochachez@gmail.com>
2019-12-31 08:41:23 -0800

    Added files of CSS, JS and React Components

---
+++ build/precache-manifest.0b5f7bc69420950b610994ffbbc167f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87cc5ac1fae36b6ec6bb56a1a5525e1e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f2e5ed59fce6a1c70e7",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "7682182d57fdc69b9671",
+    "url": "/static/js/2.3d80fa0d.chunk.js"
+  },
+  {
+    "revision": "4f2e5ed59fce6a1c70e7",
+    "url": "/static/js/main.cbcb21f0.chunk.js"
+  },
+  {
+    "revision": "4ce86dbe6b1085b21d1d",
+    "url": "/static/js/runtime-main.cd0c7a2e.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
